faed6f7d3d40bf8c737b9486751fdb9c2700ee66
Ashley Godwin <agodwin@relaypro.com>
2022-07-06 11:29:13 -0400

    Added manual documentation

--- RelayDotNet/RelayUri.cs
+++ RelayDotNet/RelayUri.cs
@@ -6,6 +6,11 @@ using System.Web;
 
 namespace RelayDotNet
 {
+    /// <summary>
+    /// The RelayUri class is responsible for defining functions that construct
+    ///  a URN from a given device or group id or name, as well as functions that parse
+    /// out certain components from a given Relay URN.
+    /// </summary>
     public class RelayUri
     {
         private const string Scheme = "urn";
@@ -24,21 +29,46 @@ namespace RelayDotNet
         private static readonly string _AllDevicesOnAcct = $"{Scheme}:{RootPath}:{All}:{Device}";
         private static readonly string _RelayUriStartsWith = $"{Scheme}:{RootPath}";
         
+        /// <summary>
+        /// Creates a URN with the given resource type, id type, and id or name.
+        /// </summary>
+        /// <param name="resourceType">indicates whether the URN is for a device, group, or interaction.</param>
+        /// <param name="idType">indicates whether the URN has an ID type of 'name' or 'id'.</param>
+        /// <param name="idOrName">the id or name of the device or group.</param>
+        /// <returns>URN containing the resource type, id type, and id or name.  Excludes the scheme and optional query.</returns>
         private static string Nidnss(string resourceType, string idType, string idOrName)
         {
             return $"{RootPath}:{idType}:{resourceType}:{HttpUtility.UrlPathEncode(idOrName)}";
         }
 
+        /// <summary>
+        /// Creates a URN from a group ID.
+        /// </summary>
+        /// <param name="groupId">the ID of the group</param>
+        /// <param name="filter">optional filter for constructing the URN.</param>
+        /// <returns>the newly constructed URN.</returns>
         public static string GroupId(string groupId, List<KeyValuePair<string, string>> filter = null)
         {
             return Construct(Group, Id, groupId, filter);
         }
         
+        /// <summary>
+        /// Creates a URN from a group name.
+        /// </summary>
+        /// <param name="groupName">the name of the group.</param>
+        /// <param name="filter">optional filter for constructing the URN.</param>
+        /// <returns>the newly constructed URN.</returns>
         public static string GroupName(string groupName, List<KeyValuePair<string, string>> filter = null)
         {
             return Construct(Group, Name, groupName, filter);
         }
 
+        /// <summary>
+        /// Creates a URN for a group member
+        /// </summary>
+        /// <param name="groupName">the name of the group that the device belongs to.</param>
+        /// <param name="deviceName">the device ID or name.</param>
+        /// <returns>the newly constructed URN.</returns>
         public static string GroupMember(string groupName, string deviceName)
         {
             var filter = new List<KeyValuePair<string, string>>()
@@ -49,21 +79,42 @@ namespace RelayDotNet
             return GroupName(groupName, filter); 
         }
         
+        /// <summary>
+        /// Creates a URN from a device ID.
+        /// </summary>
+        /// <param name="deviceId">the ID of the device.</param>
+        /// <returns>the newly constructed URN.</returns>
         public static string DeviceId(string deviceId)
         {
             return Construct(Device, Id, deviceId);
         }
 
+        /// <summary>
+        /// Creates a URN from a device name.
+        /// </summary>
+        /// <param name="deviceName">the name of the device.</param>
+        /// <returns>the newly constructed URN.</returns>
         public static string DeviceName(string deviceName)
         {
             return Construct(Device, Name, deviceName);
         }
 
+        /// <summary>
+        /// Creates a URN from an interaction name.
+        /// </summary>
+        /// <param name="name">the name of the interaction.</param>
+        /// <returns>the newly constructed URN.</returns>
         public static string InteractionName(string name)
         {
             return Construct(Interaction, Name, name);
         }
 
+        /// <summary>
+        /// Creates a URN from an interaction name and devices.
+        /// </summary>
+        /// <param name="name">the name of the interaction.</param>
+        /// <param name="devices">a list of devices.</param>
+        /// <returns>the newly constructed URN.</returns>
         public static string InteractionName(string name, IEnumerable<string> devices)
         {
             var filter = devices.Select(deviceName => new KeyValuePair<string, string>(Device, DeviceName(deviceName))).ToList();
@@ -71,6 +122,12 @@ namespace RelayDotNet
             return Construct(Interaction, Name, name, filter);
         }
 
+        /// <summary>
+        /// Returns a URN containing all of the devices with the specified status.
+        /// </summary>
+        /// <param name="interactionName">the name of the interaction.</param>
+        /// <param name="status">the status of the devices.</param>
+        /// <returns>a URN containing all of the devices with the status.</returns>
         public static string AllDevicesWithStatus(string interactionName, string status)
         {
             return Construct(Interaction, Name, interactionName, new List<KeyValuePair<string, string>>()
@@ -79,16 +136,34 @@ namespace RelayDotNet
             });
         }
 
+        /// <summary>
+        /// Retrieves all of the devices associated with the account.
+        /// </summary>
+        /// <returns>the devices.</returns>
         public static string AllDevices()
         {
             return _AllDevicesOnAcct;
         }
 
+        /// <summary>
+        /// Creates a URN containing server information.
+        /// </summary>
+        /// <returns>the newly constructed URN.</returns>
         public static string GenericOriginator()
         {
             return Construct(Server, Name, Ibot);
         }
 
+        /// <summary>
+        /// Constructs a URN based off of the resource type, id type, and
+        /// id/name.  Used by methods that need to create a URN when given a
+        /// name or ID of a device or group.
+        /// </summary>
+        /// <param name="resourceType">indicates whether the URN is for a device, group, or interaction.</param>
+        /// <param name="idType">indicates whether the URN has an ID type of 'name' of 'id'.</param>
+        /// <param name="idOrName">the id or name of the device or group.</param>
+        /// <param name="filter">optional filter.</param>
+        /// <returns>the newly constructed URN.</returns>
         private static string Construct(string resourceType, string idType, string idOrName, List<KeyValuePair<string, string>> filter = null)
         {
             if (filter is {Count: > 0})
@@ -108,6 +183,9 @@ namespace RelayDotNet
             }
         }
 
+        /// <summary>
+        /// Includes components of a Relay URN.
+        /// </summary>
         private class ParseResult
         {
             public string ResourceType { get; init; }
@@ -119,6 +197,11 @@ namespace RelayDotNet
             public List<KeyValuePair<string, string>> Filter { get; init; }
         }
 
+        /// <summary>
+        /// Helper method of parsing out a URN into its components.
+        /// </summary>
+        /// <param name="uri">the URN to parse.</param>
+        /// <returns>an array containing the components of the URN.</returns>
         private static ParseResult Parse(string uri)
         {
             if (null == uri)
@@ -172,6 +255,11 @@ namespace RelayDotNet
             }
         }
 
+        /// <summary>
+        /// Parses out a device name from a device or interaction URN.
+        /// </summary>
+        /// <param name="uri">the device or interaction URN that you would like to extract the device name from.</param>
+        /// <returns>the device name.</returns>
         public static string ParseDeviceName(string uri)
         {
             var parseResult = Parse(uri);
@@ -196,6 +284,11 @@ namespace RelayDotNet
             }
         }
 
+        /// <summary>
+        /// Parses out a device ID from a device or interaction URN.
+        /// </summary>
+        /// <param name="uri">the device or interaction URN that you would like to extract the device ID from.</param>
+        /// <returns>the device ID.</returns>
         public static string ParseDeviceId(string uri)
         {
             var parseResult = Parse(uri);
@@ -220,6 +313,11 @@ namespace RelayDotNet
             }
         }
 
+        /// <summary>
+        /// Parses out a group name from a group URN.
+        /// </summary>
+        /// <param name="uri">the URN that you would like to extract the group name from.</param>
+        /// <returns>the group name.</returns>
         public static string ParseGroupName(string uri)
         {
             var parseResult = Parse(uri);
@@ -238,6 +336,11 @@ namespace RelayDotNet
             };
         }
 
+        /// <summary>
+        /// Checks if the URN is a Relay URN.
+        /// </summary>
+        /// <param name="uri">the device, group, or interaction URN.</param>
+        /// <returns>true if the URN is a Relay URN, false otherwise.</returns>
         public static bool IsRelayUri(string uri)
         {
             return null != uri && uri.StartsWith(_RelayUriStartsWith);
